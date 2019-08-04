8fdd7ccc1c119bbd24e885096fc26735331b205c
Ghait <ghait@mogic.com>
2019-08-04 12:27:11 +0200

    setting security config and adding compiling defaultgoal

--- src/main/java/com/sepro/apigetwayservice/config/ResourceServerConfiguration.java
+++ src/main/java/com/sepro/apigetwayservice/config/ResourceServerConfiguration.java
@@ -28,12 +28,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll().and().cors().disable().csrf().disable().httpBasic().disable()
-				.exceptionHandling()
-				.authenticationEntryPoint(
-						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-				.accessDeniedHandler(
-						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+		http.authorizeRequests().anyRequest().permitAll().and().cors().disable().csrf().disable().httpBasic().disable();
 	}
 
 	@Override
